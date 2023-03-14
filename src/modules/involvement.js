e96f0592a526126dbdc61438f4f10abda158c1dc
Daniel <dselasea@gmail.com>
2023-03-14 21:43:47 +0000

    Add comment function:Error in function

--- src/modules/involvement.js
+++ src/modules/involvement.js
@@ -3,20 +3,29 @@ const likesData =
     "item_id": "1"
   }
 
+const commentsData = 
+  {
+    "item_id": "1",
+    "username": "Khemikal",
+    "comment": "I wrote this comment"
+  }
+
 const likesData2 = 
   {
     "item_id": 1
   }
+
 const postLikes = async (likes) => {
-  const response = await fetch(`https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/u2hplAfN6gX3LdTPlmsr/likes`, 
-  {
+  const response = await fetch(`https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/u2hplAfN6gX3LdTPlmsr/likes`, {
     method: 'POST',
     headers: {
-      'Content-type': 'application/json'
+      'Content-Type': 'application/json'
     },
     body: JSON.stringify(likes)
-  });
+  })
+
   const data = await response.json();
+  console.log(data);
   return data;
 }
 
@@ -25,7 +34,7 @@ const getLikes = async () => {
   {
     method: 'GET',
     headers: {
-      'Content-type': 'application/json'
+      Accept: 'application/json'
     }
   });
   const data = await response.json();
@@ -33,7 +42,36 @@ const getLikes = async () => {
   return data;
 }
 
+const postComment = async (comment) => {
+  const response = await fetch(`https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/u2hplAfN6gX3LdTPlmsr/comments`, 
+  {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(comment)
+  })
+  const data = await response.json();
+  console.log(data);
+  return data;
+}
+
+const getComment = async (item_id) => {
+  const response = await fetch(`https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/u2hplAfN6gX3LdTPlmsr/comments?item_id=${item_id}`, 
+  {
+    method: 'GET',
+    headers: {
+      Accept: 'application/json'
+    }
+  })
+
+  const data = await response.json();
+  console.log(data);
+  return data;
+}
+
 postLikes(likesData);
-postLikes(likesData2);
 getLikes();
+
+
 
