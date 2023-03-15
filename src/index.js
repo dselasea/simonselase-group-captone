396c1be98b62c8100d917562bf120564665b544b
BB-Simon <simon.chowdery@gmail.com>
2023-03-15 11:41:09 +0200

    Get pokemons and display them on the UI

--- src/index.js
+++ src/index.js
@@ -1,1 +1,24 @@
+import { getPokemons } from './modules/api.js';
 import './styles.css';
+
+const pokemonsContainer = document.getElementById('pokemons-container');
+console.log(pokemonsContainer);
+const displayPokemons = (pokemon) => {
+  const li = document.createElement('li');
+  li.classList.add('pokemon-list');
+  const html = `
+    <div>
+      <img class="pokemon-img" src=${pokemon.image} alt=${pokemon.name} />
+      <p>${pokemon.name} - ${pokemon.type}</p>
+    </div>
+  `;
+
+  li.innerHTML = html;
+  pokemonsContainer.appendChild(li);
+};
+
+(async () => {
+  const pokemons = await getPokemons();
+  pokemonsContainer.innerHTML = '';
+  pokemons.forEach((pokemon) => displayPokemons(pokemon));
+})();
