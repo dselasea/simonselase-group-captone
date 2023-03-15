4f127c72791a57de8f6623da59d08c0367efd512
Daniel <dselasea@gmail.com>
2023-03-15 09:02:47 +0000

    Fix errors

--- src/modules/involvement.js
+++ src/modules/involvement.js
@@ -1,66 +1,65 @@
-const likesData = 
-  {
-    item_id: 1
-  }
+const likesData = {
+  item_id: 1,
+};
 
+const comment = {
+  item_id: 1,
+  username: 'Khemikal',
+  comment: 'Hi from me again',
+};
 
 const postLikes = async (likes) => {
-  const response = await fetch(`https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/u2hplAfN6gX3LdTPlmsr/likes`, {
+  const response = await fetch('https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/u2hplAfN6gX3LdTPlmsr/likes', {
     method: 'POST',
     headers: {
-      'Content-Type': 'application/json'
+      'Content-Type': 'application/json',
     },
-    body: JSON.stringify(likes)
-  })
+    body: JSON.stringify(likes),
+  });
 
   const data = await response.text();
-  console.log(data);
   return data;
-}
+};
 
 const getLikes = async () => {
-  const response = await fetch(`https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/u2hplAfN6gX3LdTPlmsr/likes`, 
-  {
-    method: 'GET',
-    headers: {
-      Accept: 'application/json'
-    }
-  });
+  const response = await fetch('https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/u2hplAfN6gX3LdTPlmsr/likes',
+    {
+      method: 'GET',
+      headers: {
+        Accept: 'application/json',
+      },
+    });
   const data = await response.json();
-  console.log(data);
   return data;
-}
+};
 
 const postComment = async (comment) => {
-  const response = await fetch(`https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/u2hplAfN6gX3LdTPlmsr/comments`, 
-  {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json'
-    },
-    body: JSON.stringify(comment)
-  })
+  const response = await fetch('https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/u2hplAfN6gX3LdTPlmsr/comments',
+    {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(comment),
+    });
   const data = await response.text();
-  console.log(data);
   return data;
-}
+};
 
-const getComment = async (item_id) => {
-  const response = await fetch(`https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/u2hplAfN6gX3LdTPlmsr/comments?item_id=${item_id}`, 
-  {
-    method: 'GET',
-    headers: {
-      Accept: 'application/json'
-    }
-  })
+const getComment = async (itemId) => {
+  const response = await fetch(`https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/u2hplAfN6gX3LdTPlmsr/comments?item_id=${itemId}`,
+    {
+      method: 'GET',
+      headers: {
+        Accept: 'application/json',
+      },
+    });
 
   const data = await response.json();
-  console.log(data);
   return data;
-}
+};
 
 postLikes(likesData);
 getLikes();
-
-
-
+postComment(comment);
+getComment(1);
