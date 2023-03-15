ff5c9f82e280febfe2470061507622d26aeff681
BB-Simon <simon.chowdery@gmail.com>
2023-03-15 12:19:17 +0200

    Add like and comment icons

--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
-import { getPokemons } from './modules/api.js';
 import './styles.css';
+import '@fortawesome/fontawesome-free/js/fontawesome.js';
+import '@fortawesome/fontawesome-free/js/solid.js';
+import '@fortawesome/fontawesome-free/js/regular.js';
+import { getPokemons } from './modules/api.js';
 
 const pokemonsContainer = document.getElementById('pokemons-container');
 
@@ -10,6 +13,15 @@ const displayPokemons = (pokemon) => {
     <div>
       <img class="pokemon-img" src=${pokemon.image} alt=${pokemon.name} />
       <p>${pokemon.name} - ${pokemon.type}</p>
+      <p>
+        <i class="fa-regular fa-heart"></i> Likes ${5}
+      </p>
+      <p>
+        <i class="fa-regular fa-comment"></i>
+        <button>Comments</button>
+      </p>
+
+
     </div>
   `;
 
