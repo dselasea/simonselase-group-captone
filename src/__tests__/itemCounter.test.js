3bc8e26b4c1520a43672944d08b2fd135f12c618
BB-Simon <simon.chowdery@gmail.com>
2023-03-16 06:11:17 +0200

    Add test for item counter

---
+++ src/__tests__/itemCounter.test.js
@@ -0,0 +1,26 @@
+/**
+ * @jest-environment jsdom
+ */
+
+import itemCounter from '../modules/itemCounter.js';
+
+describe('Pokemon counter', () => {
+  test('It should returns the number of child elements in #pokemons-container', () => {
+    document.body.innerHTML = `
+      <ul id="pokemons-container">
+        <li>Pokemon 1</li>
+        <li>Pokemon 2</li>
+        <li>Pokemon 3</li>
+      </ul>
+    `;
+    const pokemonContainer = document.getElementById('pokemons-container');
+    const count = itemCounter(pokemonContainer);
+    expect(count).not.toBeNull();
+    expect(count).not.toBeUndefined();
+    expect(count).not.toBeFalsy();
+    expect(count).not.toBeGreaterThan(3);
+    expect(count).not.toBeLessThan(3);
+    expect(count).toBe(3);
+    expect(count).toEqual(3);
+  });
+});
